f30fa51c67f78ffc1bab0f487143d5eba0b6f816
Kevin Ahn and nick brown <nick@>
2018-06-12 09:33:10 -0700

    add page inputs, constructor

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+ function MovieTicket(name,movie,time,age){
+   this.name = name
+   this.movie = movie
+   this.time = time
+   this.age = age
+ }
+
+
+
+
+
+
+ $(document).ready(function(){
+  var inputtedName = $("#inputName").val();
+  var inputtedMovieSelection = $("#movieSelection").val();
+  var inputtedTimeSelection = $("#timeSelection").val();
+  var inputttedAge = parseInt($("#ageSelection").val();
+
+  
+
+
+});
